0d55b7c1a0c7591a590b14ee2e6f69f6fcc2221e
raselASrock <mdraselm413@gmail.com>
2022-08-03 22:39:39 +0600

    paramiter-added

--- paramiter.js
+++ paramiter.js
@@ -3,3 +3,24 @@ function bringSingara(money){
     console.log(money);
 }
 bringSingara(250);
+// *****************************
+
+function bringPizza(money){
+    console.log("Pizzar dam", money);
+    console.log("Aie je apner pizza nen");
+}
+bringPizza(100);
+
+// ************************
+function add(num1, num2){
+    console.log("Going to add:", num1, num2);
+}
+add(45, 75);
+
+// **************
+
+function sum(a, b, c, d, e){
+    var sum = a + b + c + d + e ;
+    console.log(sum);
+}
+sum(69, 85, 25, 15, 12);
