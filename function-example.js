a9538f8b660544f3a8d76a18d892fa6c354409b5
raselASrock <mdraselm413@gmail.com>
2022-08-04 01:44:39 +0600

    function-added

--- function-example.js
+++ function-example.js
@@ -1,3 +1,16 @@
-function functionName (parameters){
-    // function body
+// function functionName (parameters){
+//     // function body
+//     // return
+// }
+// functionName(parameters value)
+
+function getAverage(assignment1, assignment2, assignment3){
+    const total = assignment1 + assignment2 + assignment3 ;
+    const average = total / 3 ;
+    return average
 }
+const assignment1Mark = 60;
+const assignment2Mark = 37;
+const assignment3Mark = 60;
+var myAverage = getAverage(assignment1Mark, assignment2Mark, assignment3Mark);
+console.log("My Assignment so far:", myAverage);
