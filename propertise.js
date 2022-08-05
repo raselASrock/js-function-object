6e47fc53e01af3a5682a2f56b8a4c8ccb1644ff0
raselASrock <mdraselm413@gmail.com>
2022-08-05 23:20:30 +0600

    new code

---
+++ propertise.js
@@ -0,0 +1,12 @@
+var shopingCart = {
+    books: 3,
+    sunglass: 1,
+    keyboard: 5,
+    mouse: 1,
+    pen: 25
+}
+var penCount= shopingCart.pen;
+var penCount2 = shopingCart["pen"];
+console.log(shopingCart);
+console.log(penCount);
+console.log(penCount2);
