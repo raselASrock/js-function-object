cccfb46a3ba663236a275d01b0d9a21a65eef236
raselASrock <mdraselm413@gmail.com>
2022-08-05 23:08:29 +0600

    object add

---
+++ object.js
@@ -0,0 +1,24 @@
+var student = {
+    id: 115, 
+    name: "Solaiman-Khan", 
+    class: 9, 
+    marks: 98
+}
+
+var mobile = {
+    brand: "Samsung",
+    price: 19000,
+    storage:"64 GB",
+    camera: "50mp"
+}
+
+var myComputer = {
+    brand: "Dell",
+    price: 120000,
+    color: "black",
+    processor: "Ryzen 5 5600G",
+
+}
+
+console.log(myComputer);
+console.log(myComputer.price);
