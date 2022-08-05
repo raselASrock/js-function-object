f8fc9f0f16f965686549cf62974385ae839ea466
raselASrock <mdraselm413@gmail.com>
2022-08-05 23:38:08 +0600

    object-name

--- propertise.js
+++ propertise.js
@@ -10,3 +10,11 @@ var penCount2 = shopingCart["pen"];
 console.log(shopingCart);
 console.log(penCount);
 console.log(penCount2);
+var propertise = Object.keys(shopingCart);
+var propertyValues = Object.values(shopingCart);
+console.log(propertise);
+console.log(propertyValues);
+
+var propertyName = "mouse";
+var propertyValues = shopingCart[propertyName];
+console.log(propertyValues);
